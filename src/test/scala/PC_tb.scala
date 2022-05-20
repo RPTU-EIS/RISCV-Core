c94db91d30f77d5ae79957d82cc9b2e67362cc2b
Giorgi Solomnishvili <solomni@rhrk.uni-kl.de>
2022-05-20 16:57:43 +0200

    add test for PC

---
+++ src/test/scala/PC_tb.scala
@@ -0,0 +1,28 @@
+
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+import PC._
+import chisel3._
+
+
+class PC_tb extends AnyFlatSpec with ChiselScalatestTester {
+  "GPR" should "pass" in {
+    test(new PC).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      dut.io.PC_next.poke(20.U(32.W)) // write 5 to reg 15
+      dut.io.WE.poke(true.B)
+
+      dut.clock.step()
+
+      dut.io.WE.poke(false.B)
+      dut.io.PC_current.expect(20.U(32.W))
+
+      dut.clock.step()
+
+      dut.io.PC_current.expect(20.U(32.W))
+
+      dut.reset.poke(true.B)
+      dut.clock.step()
+      dut.io.PC_current.expect(0.U(32.W))
+    }
+  }
+}

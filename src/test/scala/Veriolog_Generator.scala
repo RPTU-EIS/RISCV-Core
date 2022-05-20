d70f733b81edd34f9e8c9747fef325be00e153de
Giorgi Solomnishvili <solomni@rhrk.uni-kl.de>
2022-05-20 16:48:50 +0200

    Add program counter

--- src/test/scala/Veriolog_Generator.scala
+++ src/test/scala/Veriolog_Generator.scala
@@ -1,10 +1,11 @@
 
 import ALU._
 import GPR._
-
+import PC._
 object VerilogGen extends App
 {
   (new chisel3.stage.ChiselStage).emitVerilog(new add_subtractor())
   (new chisel3.stage.ChiselStage).emitVerilog(new ALU())
   (new chisel3.stage.ChiselStage).emitVerilog(new registerFile())
+  (new chisel3.stage.ChiselStage).emitVerilog(new PC())
 }

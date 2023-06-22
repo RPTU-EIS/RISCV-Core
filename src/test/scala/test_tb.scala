116e117c419cd7179319d0bd2184b75b20a61781
tsotnek <tsotne.karchava04@gmail.com>
2023-06-22 14:28:44 +0200

    updated binarytester script

--- src/test/scala/test_tb.scala
+++ src/test/scala/test_tb.scala
@@ -10,7 +10,7 @@ import java.sql.Driver
 class scala_Test_tb extends AnyFlatSpec with ChiselScalatestTester {
 
    "Scala_test" should "pass" in {
-     test(new RISCV_TOP("src/main/scala/InstructionMemory/mulhsutest")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+     test(new RISCV_TOP("src/test/programs/hexfile")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
       for(i <- 0 until 200){
         dut.clock.step()
       }

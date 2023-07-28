9527af4c724b65baf501ceb0133ee827ef84b75d
Tobias Jauch <jauch@eit.uni-kl.de>
2023-07-28 10:26:51 +0200

    Branch resolution for wrong target predictions (JALR) (#9)
    activate branch hazard resolution when predicted target address was wrong (JALR)

--- src/main/scala/Stage_EX/EX.scala
+++ src/main/scala/Stage_EX/EX.scala
@@ -43,18 +43,18 @@ class EX extends Module {
       val btbTargetPredict   = Input(UInt(32.W))
       val newBranch          = Output(Bool())
       val updatePrediction   = Output(Bool())
-      val updateTarget       = Output(Bool())
       val outPCplus4         = Output(UInt(32.W))
       val ALUResult          = Output(UInt(32.W))
       val branchTarget       = Output(UInt(32.W))
-      val branchCond         = Output(Bool())
+      val branchTaken        = Output(Bool())
+      val wrongAddrPred      = Output(Bool())
       val Rs2Forwarded       = Output(UInt(32.W))
     }
   )
 
-  val ALU          = Module(new ALU).io
-  val MDU          = Module(new MDU).io
-  val Branch       = Module(new Branch_OP).io
+  val ALU                 = Module(new ALU).io
+  val MDU                 = Module(new MDU).io
+  val ResolveBranch       = Module(new Branch_OP).io
 
 
   val mdu_op_flag             = Wire(Bool())
@@ -64,39 +64,34 @@ class EX extends Module {
   val alu_operand2            = Wire(UInt())
   val PCplus4 = Wire(UInt(32.W))
   // Control signals to ALU and Branch
-  Branch.branchType := io.branchType
-  ALU.ALUop         :=io.ALUop
-
-
-
-  Branch.branchType := io.branchType
-  ALU.ALUop         :=io.ALUop
+  ResolveBranch.branchType := io.branchType
+  ALU.ALUop                :=io.ALUop
 
   // Choosing ALU and Branch_Op Inputs  -- 2 consecutive MUXes
     //Forwarded operands -- 1st MUX
   when(io.rs1Select === 1.asUInt(2.W)){
-    alu_operand1  := io.ALUresultEXB
-    Branch.src1   := io.ALUresultEXB
+    alu_operand1         := io.ALUresultEXB
+    ResolveBranch.src1   := io.ALUresultEXB
   }
   .elsewhen(io.rs1Select === 2.asUInt(2.W)){
-    alu_operand1  := io.ALUresultMEMB
-    Branch.src1   := io.ALUresultMEMB
+    alu_operand1         := io.ALUresultMEMB
+    ResolveBranch.src1   := io.ALUresultMEMB
   }
   .otherwise{
-    alu_operand1  := io.rs1
-    Branch.src1   := io.rs1
+    alu_operand1         := io.rs1
+    ResolveBranch.src1   := io.rs1
   }
   when(io.rs2Select === 1.asUInt(2.W)){
-    alu_operand2  := io.ALUresultEXB
-    Branch.src2   := io.ALUresultEXB
+    alu_operand2         := io.ALUresultEXB
+    ResolveBranch.src2   := io.ALUresultEXB
   }
   .elsewhen(io.rs2Select === 2.asUInt(2.W)){
-    alu_operand2  := io.ALUresultMEMB
-    Branch.src2   := io.ALUresultMEMB
+    alu_operand2         := io.ALUresultMEMB
+    ResolveBranch.src2   := io.ALUresultMEMB
   }
   .otherwise{
-    alu_operand2  := io.rs2
-    Branch.src2   := io.rs2
+    alu_operand2         := io.rs2
+    ResolveBranch.src2   := io.rs2
   }
     //Operand 1, 2nd Mux
   when(io.op1Select === op1sel.PC){
@@ -121,8 +116,10 @@ class EX extends Module {
 
 
   // EX stage outputs
-  io.branchCond   := Branch.branchCondition
-  io.branchTarget := ALU.aluRes
+  io.branchTaken   := ResolveBranch.branchTaken // Branch taken or not in sequential program flow?
+  io.wrongAddrPred := io.btbHit && (ALU.aluRes =/= io.btbTargetPredict) // hit but target addr in BTB was incorrect
+  io.branchTarget  := ALU.aluRes  // calculated branch target
+  
 
   //assert((ALU.aluRes === io.btbTargetPredict),"BTB hit, but predicted branch target doesn't match!")
 
@@ -137,23 +134,16 @@ class EX extends Module {
 
   // BTB-related: Finding new Branch Instructions and Updating Existing Prediction
   when(io.branchType =/= branch_types.DC){ // In case instruction is a valid branch (valid means not flushed)
-    when(!io.btbHit){ // In case of BTB miss, send this as new BTB entry to IF stage
-      io.newBranch        := 1.B  // Update BTB! -> Tells IF to take io.branchTarget as entryBrTarget AND take IDBarrier.io.outPC as entryPC
-      io.updateTarget     := 0.B
-      io.updatePrediction := 0.B
-    }.elsewhen(ALU.aluRes =/= io.btbTargetPredict) {
-      io.newBranch        := 0.B
-      io.updateTarget     := 1.B
-      io.updatePrediction := 1.B
+    when(!io.btbHit || (io.btbHit && (ALU.aluRes =/= io.btbTargetPredict))){ // In case of BTB miss, or BTB hit, but wrong target address (may occur only for JALR) send this as new BTB entry to IF stage
+      io.newBranch        := true.B  // Update BTB! -> Tells IF to take io.branchTarget as entryBrTarget AND take IDBarrier.io.outPC as entryPC
+      io.updatePrediction := false.B
     }otherwise{ // In case of BTB hit (we already know this branch), tell IF to change prediction FSM
-      io.newBranch        := 0.B
-      io.updatePrediction := 1.B
-      io.updateTarget     := 0.B
+      io.newBranch        := false.B
+      io.updatePrediction := true.B
     }
   }.otherwise{
-    io.newBranch        := 0.B
-    io.updatePrediction := 0.B
-    io.updateTarget     := 0.B
+    io.newBranch        := false.B
+    io.updatePrediction := false.B
   }
   io.outPCplus4 := PCplus4
 }
--- src/main/scala/Stage_IF/BTB_direct.scala
+++ src/main/scala/Stage_IF/BTB_direct.scala
@@ -19,11 +19,10 @@ class BTB_direct extends Module {
   
   val io = IO(new Bundle {
     val currentPC           = Input(UInt(32.W))
-    val newBranch          = Input(Bool())
+    val newBranch           = Input(Bool())
     val entryPC             = Input(UInt(32.W))
     val entryBrTarget       = Input(UInt(32.W))
     val branchMispredicted  = Input(Bool())       // Acts as WrEn for Predictor Array
-    val branchBehavior      = Input(Bool())       // 1 means Taken -- 0 means Not Taken 
     val updatePrediction    = Input(Bool())
     val prediction          = Output(Bool())      // 1 means Taken
     val btbHit              = Output(Bool())      // 1 means Hit
@@ -50,61 +49,80 @@ class BTB_direct extends Module {
   // Array of 2bit FSMs -- state bits. It is indexed by currentPC[7:2]
   val predictorArray = Mem(64, UInt(2.W)) 
   val predictorOut = Wire(UInt(2.W))
-  
+
   // Avoiding a simulation artifact during reset
-  val btbWrEn = Wire(Bool())
+  val btbWriteNewEntry = Wire(Bool())
   when(Module.reset.asBool){
-    btbWrEn := 0.B
+    btbWriteNewEntry := false.B
   }.otherwise{
-    btbWrEn := io.newBranch && io.branchMispredicted  // Collecting new Branches only if mispredicted (i.e., they are taken!)
+    btbWriteNewEntry := io.newBranch && io.branchMispredicted  // Collecting new Branches only if mispredicted (i.e., they are taken!)
   }
-  
+
   // Initialize BTB and Prediction Array
   var BTB_Init:String = "src/main/scala/Stage_IF/BTB_Init"
   var Predictor_Init:String = "src/main/scala/Stage_IF/Predictor_Init"
   loadMemoryFromFileInline(btb, BTB_Init,  MemoryLoadFileType.Hex)
   loadMemoryFromFileInline(predictorArray, Predictor_Init, MemoryLoadFileType.Binary)
 
-  // Read BTB
+
+  // --------------------------------------------------------------
+  // READ FROM BTB
+  // --------------------------------------------------------------
+
   val btbOut = WireInit(btb(io.currentPC(7,2)))
   btbOutput.valid := btbOut(54)
   btbOutput.Tag := btbOut(53, 30)
   btbOutput.branchTarget := btbOut(29, 0)
   io.targetAdr := Cat(btbOutput.branchTarget, 0.U(2.W))
-  when( btbOutput.valid === 1.B && btbOutput.Tag === io.currentPC(31,8) ){
-    io.btbHit := 1.B  // Hit
+  when( btbOutput.valid === true.B && btbOutput.Tag === io.currentPC(31,8) ){
+    io.btbHit := true.B  // Hit
   }
   .otherwise{
-    io.btbHit := 0.B  // Miss
-  }
-  // Read Predictor
-  predictorOut := predictorArray(io.currentPC(7,2))
-  when( io.btbHit && (predictorOut === strongTaken || predictorOut === weakTaken) ){
-    io.prediction := 1.B  // Taken
-  }
-  .otherwise{
-    io.prediction := 0.B  // Not Taken. It is the default when BTB misses too
+    io.btbHit := false.B  // Miss
   }
 
-  // Write BTB
+
+  // --------------------------------------------------------------
+  // READ PREDICTION
+  // --------------------------------------------------------------
+
+  predictorOut := predictorArray(io.currentPC(7,2))
+  when( io.btbHit && (predictorOut === strongTaken || predictorOut === weakTaken) ){
+    io.prediction := true.B  // Taken
+  }
+  .otherwise{
+    io.prediction := false.B  // Not Taken. It is the default when BTB misses too
+  }
+
+
+  // --------------------------------------------------------------
+  // WRITE TO BTB
+  // --------------------------------------------------------------
+
   btbInput.Tag := io.entryPC(31,8)
   btbInput.branchTarget := io.entryBrTarget(31,2)
-  btbInput.valid := 0.B // Default value, it's a wire
-  when(btbWrEn === 1.B){
-    btbInput.valid := 1.B
+  btbInput.valid := false.B // Default value, it's a wire
+  
+  when(btbWriteNewEntry){
+    btbInput.valid := true.B
     btb(io.entryPC(7,2)) := btbInput.asUInt
   }
-  // Write Predictor
+
+
+  // --------------------------------------------------------------
+  // UPDATE 2-BIT FSM FOR PREDICTION
+  // --------------------------------------------------------------
+
   val prevPrediction1 = RegNext(predictorOut)
   val prevPrediction2 = RegNext(prevPrediction1)  // This register holds the prediction from 2 cycles before. It is used for updating Prediction after EX stage calculates branch behavior
-  when(btbWrEn === 1.B){
+  when(btbWriteNewEntry === true.B){
     predictorArray(io.entryPC(7,2)) := strongTaken  // *Note*: what's the best initial state? Strong taken helps with Loops
   }
-  .elsewhen(io.updatePrediction === 1.B){
+  .elsewhen(io.updatePrediction === true.B){
     // Prediction FSMs next state logic
-    switch ( prevPrediction2 ) { // Switch on the Current State
+    switch (prevPrediction2) { // Switch on the Current State
       is(strongNotTaken) {
-        when(io.branchMispredicted === 1.B){
+        when(io.branchMispredicted === true.B){
           predictorArray(io.entryPC(7,2)) := weakNotTaken
         }
         .otherwise{
@@ -112,7 +130,7 @@ class BTB_direct extends Module {
         }
       }
       is(weakNotTaken) {
-        when( io.branchMispredicted === 1.B ){
+        when(io.branchMispredicted === true.B){
           predictorArray(io.entryPC(7,2)) := weakTaken
         }
         .otherwise{
@@ -120,7 +138,7 @@ class BTB_direct extends Module {
         }
       }
       is(strongTaken) {
-        when( io.branchMispredicted === 1.B ){
+        when(io.branchMispredicted === true.B){
           predictorArray(io.entryPC(7,2)) := weakTaken
         }
         .otherwise{
@@ -128,7 +146,7 @@ class BTB_direct extends Module {
         }
       }
       is(weakTaken) {
-        when( io.branchMispredicted === 1.B ){
+        when(io.branchMispredicted === true.B){
           predictorArray(io.entryPC(7,2)) := weakNotTaken
         }
         .otherwise{
